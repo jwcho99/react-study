a0b8430495ab142d98ca5e37bd7d139986f8532a
jwcho <jwcho1999@naver.com>
2024-06-01 21:24:59 +0900

    WIP: ac9b06a Revert "Enhance code"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,11 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+<<<<<<< Updated upstream
     console.log('abc')
 
+=======
+>>>>>>> Stashed changes
     return (
         <main
             className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
